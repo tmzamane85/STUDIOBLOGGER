dfb07575d33ae7b86f65c42e4ec01145be969fff
Thabang <thabangmzamane@outlook.com>
2020-03-09 21:27:08 +0200

    Alpha Blog UI design with Bootstrap

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,54 @@
+class ArticlesController < ApplicationController
+    
+    
+    def index
+        @articles = Article.all
+    end
+
+    def home
+
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = "Article created successfully"
+            redirect_to article_path(@article)
+        else
+         render 'new'
+        end
+    end 
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+        flash[:notice]= "Article updated successfully"
+        redirect_to edit_article_path
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice]= "Article was successfully deleted"
+        redirect_to articles_path
+    end
+end
+    private 
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end    
+    
+
